c7230e17b378167823cca95b60cca2907983a52c
Leandro Lisboa Penz <lpenz@lpenz.org>
2021-07-01 21:56:09 +0100

    README.md: add doc; create version_tagged, version_commit

--- README.md
+++ README.md
@@ -3,5 +3,85 @@
 
 # ghaction-githeadinfo
 
-Github action that outputs pieces of information about the current
-HEAD to use as part of a version
+ghaction-githeadinfo is a docker github action that outputs a version
+number for you to use in a deploy action.
+
+There are many ways to generate version information for a
+repository. They usually involve processing `GITHUB_REF` in some
+[way](https://stackoverflow.com/questions/58177786/get-the-current-pushed-tag-in-github-actions),
+maybe using even using [github-script](https://github.com/actions/github-script).
+
+This repository is also an example of how to create a docker github
+action that compiles a rust entrypoint in a container and then moves
+it to a second, minimal container.
+
+
+## Outputs
+
+The following are the *primary* outputs of this action, usually the
+ones used for versioning:
+
+- `version_tagged`: for repositories that should only deploy on tags,
+  it's defined if the github event was a push of a tag.
+
+  The output itself is the tag, with the optional `v` stripped.
+
+- `version_commit`: for repositories that deploy on all commits to
+  `master` or `main`, it's defined if the github event was a push to
+  one of those.
+
+  The output itself is the most recent tag on the branch, with the
+  optional `v` stripped, followed by the distance between the branch
+  and the tag.
+
+
+The idea of this scheme is to allow the user to check if
+`version_tagged` or `version_commit` is not empty, and in this case
+use it as the version being deployed:
+
+```{yml}
+jobs:
+  deploy:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v2
+      - id: version
+        uses: docker://lpenz/ghaction-githeadinfo:v0.2
+      - name: deploy
+        uses: <deploy action>
+        if: steps.version.version_tagged != ''
+        with:
+          version: ${{ steps.version.version_tagged }}
+```
+
+The same thing works for `version_commit`. Just keep in mind that, for
+it to work on a tagged commit, you may have to push the tag before the
+commit, which is counter-intuitive.
+
+
+### Secondary outputs
+
+These are the *secondary* outputs that might be useful for debugging
+or as alternative versioning schemes:
+
+- `is_push`: if the github event was identified, "true" if the event
+  was a push or "false" otherwise.
+- `is_tag`: if the github ref was identified, "true" if the ref is a
+  tag, false otherwise.
+- `is_main`: "true" if the github ref was for a branch named `main` or
+  `master`.
+- `is_push_tag`: "true" if a tag was pushed.
+- `is_push_main`: "true" if `main` or `master` were pushed.
+- `commit`: the hash of the commit.
+- `git_describe_tags`: the output of `git describe --tags`
+- `tag_latest`: the most recent tag.
+- `distance`: the distance between the current commit and `tag_latest`.
+- `tag_distance`: `tag_latest-distance`
+- `tag_head`: the tag on HEAD, if there's a tag on HEAD (does not
+  depend on the gitub event).
+- `dash_distance`: `-` prepended to `distance`
+- `tag_latest_ltrimv`: `tag_latest` without the optional leading `v`.
+- `tag_head_ltrimv`: `tag_head` without the optionsl leading `v`, if
+  `tag_head` was defined.
+- `version_tagged`: `tag_head_ltrimv` if `is_push_tag`.
+- `version_commit`: `tag_distance_ltrimv` if `is_push_main`.
--- src/lib.rs
+++ src/lib.rs
@@ -12,17 +12,46 @@ use regex::Regex;
 
 #[derive(Debug, Default, Clone)]
 pub struct Info {
+    pub is_push: Option<bool>,
+    pub is_tag: Option<bool>,
+    pub is_main: Option<bool>,
+    pub is_push_tag: Option<bool>,
+    pub is_push_main: Option<bool>,
     pub commit: String,
     pub git_describe_tags: String,
     pub tag_latest: String,
     pub distance: String,
-    pub tag_head: Option<String>,
     pub dash_distance: String,
+    pub tag_distance: String,
+    pub tag_head: Option<String>,
     pub tag_latest_ltrimv: String,
+    pub tag_distance_ltrimv: String,
     pub tag_head_ltrimv: Option<String>,
+    pub version_tagged: Option<String>,
+    pub version_commit: Option<String>,
 }
 
 impl Info {
+    pub fn parse_env(&mut self, enviter: impl Iterator<Item = (String, String)>) {
+        for (k, v) in enviter {
+            if k == "GITHUB_EVENT_NAME" {
+                self.is_push = Some(v == "push");
+            }
+            if k == "GITHUB_REF" {
+                self.is_tag = Some(v.starts_with("refs/tags/"));
+                self.is_main = Some(v == "refs/heads/main" || v == "refs/heads/master");
+            }
+        }
+        self.is_push_tag = match (self.is_push, self.is_tag) {
+            (Some(a), Some(b)) => Some(a && b),
+            _ => None,
+        };
+        self.is_push_main = match (self.is_push, self.is_main) {
+            (Some(a), Some(b)) => Some(a && b),
+            _ => None,
+        };
+    }
+
     pub fn parse_describe(&mut self, s0: impl AsRef<str>) -> Result<()> {
         let s = s0.as_ref();
         self.git_describe_tags = s.into();
@@ -36,10 +65,18 @@ impl Info {
             self.tag_head = Some(s.into());
         }
         self.dash_distance = format!("-{}", self.distance);
+        self.tag_distance = format!("{}{}", self.tag_latest, self.dash_distance);
         let re = Regex::new(r"^v?(?P<tag_ltrimv>.*)$")?;
         self.tag_latest_ltrimv = re.replace(&self.tag_latest, "$tag_ltrimv").into();
+        self.tag_distance_ltrimv = re.replace(&self.tag_distance, "$tag_ltrimv").into();
         if let Some(ref tag_head) = self.tag_head {
             self.tag_head_ltrimv = Some(re.replace(tag_head, "$tag_ltrimv").into());
+        }
+        if self.is_push_tag == Some(true) {
+            self.version_tagged = self.tag_head_ltrimv.clone();
+        }
+        if self.is_push_main == Some(true) {
+            self.version_commit = Some(self.tag_distance_ltrimv.clone());
         }
         Ok(())
     }
@@ -50,6 +87,7 @@ impl Info {
             commit: git::head_commit()?,
             ..Info::default()
         };
+        info.parse_env(std::env::vars());
         if let Ok(gitdescr) = git::describe() {
             info.parse_describe(gitdescr)?;
         }
@@ -58,24 +96,47 @@ impl Info {
 }
 
 impl<'a> IntoIterator for &'a Info {
-    type Item = (&'static str, &'a str);
-    type IntoIter = std::vec::IntoIter<(&'static str, &'a str)>;
+    type Item = (&'static str, String);
+    type IntoIter = std::vec::IntoIter<(&'static str, String)>;
     fn into_iter(self) -> Self::IntoIter {
-        let mut v = vec![
-            ("commit", self.commit.as_str()),
-            ("git_describe_tags", self.git_describe_tags.as_str()),
-            ("tag_latest", self.tag_latest.as_str()),
-            ("distance", self.distance.as_str()),
-            ("dash_distance", self.dash_distance.as_str()),
-            ("tag_latest_ltrimv", self.tag_latest_ltrimv.as_str()),
+        let mut vec: Vec<(&'static str, String)> = vec![
+            ("commit", self.commit.clone()),
+            ("git_describe_tags", self.git_describe_tags.clone()),
+            ("tag_latest", self.tag_latest.clone()),
+            ("distance", self.distance.clone()),
+            ("dash_distance", self.dash_distance.clone()),
+            ("tag_distance", self.dash_distance.clone()),
+            ("tag_latest_ltrimv", self.tag_latest_ltrimv.clone()),
+            ("tag_distance_ltrimv", self.tag_latest_ltrimv.clone()),
         ];
+        if let Some(v) = &self.is_push {
+            vec.push(("is_push", format!("{}", v)));
+        }
+        if let Some(v) = &self.is_tag {
+            vec.push(("is_tag", format!("{}", v)));
+        }
+        if let Some(v) = &self.is_main {
+            vec.push(("is_main", format!("{}", v)));
+        }
+        if let Some(v) = &self.is_push_tag {
+            vec.push(("is_push_tag", format!("{}", v)));
+        }
+        if let Some(v) = &self.is_push_main {
+            vec.push(("is_push_main", format!("{}", v)));
+        }
         if let Some(t) = &self.tag_head {
-            v.push(("tag_head", t.as_str()));
+            vec.push(("tag_head", t.into()));
         }
         if let Some(t) = &self.tag_head_ltrimv {
-            v.push(("tag_head_ltrimv", t.as_str()));
+            vec.push(("tag_head_ltrimv", t.into()));
         }
-        v.into_iter()
+        if let Some(t) = &self.version_tagged {
+            vec.push(("version_tagged", t.into()));
+        }
+        if let Some(t) = &self.version_commit {
+            vec.push(("version_commit", t.into()));
+        }
+        vec.into_iter()
     }
 }
 
